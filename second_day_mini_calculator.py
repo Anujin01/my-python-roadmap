e525a4d2c5b259320182b8a7957fdfdebc0320ef
Anujin Munkhsukh <anujinmunkhsukh@gmail.com>
2025-11-02 07:40:49 -0500

    Uncomment user name input and greeting
    Uncommented name input and greeting print statements.

---
+++ second_day_mini_calculator.py
@@ -0,0 +1,21 @@
+# input() - function that prompts the user to input data and
+#           returns the entered data as a string
+
+name = input("Hello my name is Anujin, What is your name? ")
+print(f"Nice you meet you {name} ")
+
+
+# I'll try mini calculator here
+
+a = float(input("Please enter your number (a): "))
+b = float(input("Please enter your number (b): "))
+operator = input("Please choose your operating tool(*,+,-,/): ")
+
+if operator == "*":
+    print(a * b)
+if operator == "+":
+    print(a+b)
+if operator == "-":
+    print(a-b)
+if operator == "/":
+    print(a/b)
