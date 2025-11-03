cf110843019a55af6aaac4a3db2c7b62d4b3a5e2
Anujin Munkhsukh <anujinmunkhsukh@gmail.com>
2025-11-03 15:46:07 -0500

    Add age check functionality

---
+++ day3_age_check.py
@@ -0,0 +1,10 @@
+
+age = int(input("please, enter your age: "))
+if age <= 12:
+    print("Hello, you are a kid.")
+elif age <= 19:
+    print("Hello, you are a teenager.")
+elif age <= 100:
+    print("Hello, you are a adult.")
+else:
+    print("Please, enter correct age ")
